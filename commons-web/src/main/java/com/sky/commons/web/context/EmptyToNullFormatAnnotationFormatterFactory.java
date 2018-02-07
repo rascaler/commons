5c92ec90a323d48de68bcde3106aa8c8c6be0ea0
rascaler <rascaler@163.com>
2018-02-07 10:07:39 +0800

    【添加】web基础类

---
+++ commons-web/src/main/java/com/sky/commons/web/context/EmptyToNullFormatAnnotationFormatterFactory.java
@@ -0,0 +1,55 @@
+package com.sky.commons.web.context;
+
+import com.sky.commons.utils.annotation.EmptyToNullFormat;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.context.support.EmbeddedValueResolutionSupport;
+import org.springframework.format.AnnotationFormatterFactory;
+import org.springframework.format.Formatter;
+import org.springframework.format.Parser;
+import org.springframework.format.Printer;
+
+import java.text.ParseException;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Set;
+
+public class EmptyToNullFormatAnnotationFormatterFactory extends EmbeddedValueResolutionSupport
+        implements AnnotationFormatterFactory<EmptyToNullFormat> {
+
+
+    @Override
+    public Set<Class<?>> getFieldTypes() {
+        Set<Class<?>> clazz = new HashSet<Class<?>>();
+        clazz.add(String.class);
+        return clazz;
+    }
+
+    @Override
+    public Printer<String> getPrinter(EmptyToNullFormat emptyToNullFormat, Class<?> aClass) {
+        return this.configureFormatterFrom(emptyToNullFormat);
+    }
+
+    @Override
+    public Parser<String> getParser(EmptyToNullFormat emptyToNullFormat, Class<?> aClass) {
+        return this.configureFormatterFrom(emptyToNullFormat);
+    }
+    private Formatter<String> configureFormatterFrom(EmptyToNullFormat annotation) {
+        return new Formatter<String>() {
+            @Override
+            public String parse(String s, Locale locale) throws ParseException {
+                if(StringUtils.isEmpty(s))
+                    return null;
+                else
+                    return s;
+            }
+
+            @Override
+            public String print(String s, Locale locale) {
+                if(StringUtils.isEmpty(s))
+                    return null;
+                else
+                    return s;
+            }
+        };
+    }
+}
